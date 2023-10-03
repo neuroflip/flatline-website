52705f72279a935e5d0b1b833a6d2706af371eb1
José Ángel Anguita Rovira <jaanguita@MacBook-Pro-de-Jose.local>
2023-10-03 18:50:28 +0200

    Simplified the ColumnDataProvider mock

--- src/__mocks__/ColumnDataProvider.ts
+++ src/__mocks__/ColumnDataProvider.ts
@@ -3,19 +3,9 @@ import type { ColumnDataProviderProps } from '../types/ColumnDataProvider'
 const ColumnDataProvider = jest.fn().mockImplementation(() => {
   return {
     getData: jest.fn().mockImplementation(({ col1Classes, col2Classes, index }: ColumnDataProviderProps): TrustedHTML[] => {
-      let col1
-      let col2
-      let col3
-
-      if (index === 0) {
-        col1 = `<div>column5 ${index}</div>`
-        col2 = `<div>column1 ${index}</div>`
-        col3 = `<div>column2 ${index}</div>`
-      } else {
-        col1 = `<div>column1 ${index}</div>`
-        col2 = `<div>column2 ${index}</div>`
-        col3 = `<div>column3 ${index}</div>`
-      }
+      const col1 = index === 0 ? '<div>column5 0</div>' : '<div>column1 1</div>'
+      const col2 = index === 0 ? '<div>column1 0</div>' : '<div>column2 1</div>'
+      const col3 = index === 0 ? '<div>column2 0</div>' : '<div>column3 1</div>'
 
       return [col1, col2, col3]
     })
