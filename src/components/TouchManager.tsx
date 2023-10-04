6ab939c012e5da44fe6ab4e3ca02ea284ca29c53
José Ángel Anguita Rovira <jaanguita@MacBook-Pro-de-Jose.local>
2023-10-04 17:02:59 +0200

    Resolved a bug with setState that reacts companied about, changed setState inside touchmanager with an inmutable useRef state

--- src/components/TouchManager.tsx
+++ src/components/TouchManager.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import type { TouchManagerProps, TouchManagerState } from '../types/TouchManager'
+import type { TouchManagerProps } from '../types/TouchManager'
 
 const body = window.document.body
 const MIN_SENSI = 100
@@ -15,7 +15,7 @@ function TouchManager ({
   onResetColumns,
   children
 }: TouchManagerProps): React.JSX.Element {
-  const [state, setState] = React.useState({ startX: 0, startY: 0, isRealEvent: false })
+  const stateRef = React.useRef({ startX: 0, startY: 0, isRealEvent: false })
 
   React.useEffect(() => {
     addEvents()
@@ -41,79 +41,64 @@ function TouchManager ({
   }
 
   function onTouchStart (nativeEvent: TouchEvent | MouseEvent): void {
-    setState((state: TouchManagerState) => {
-      const newState = { ...state }
+    if (nativeEvent instanceof TouchEvent) {
+      const touches = nativeEvent.touches
 
-      if (nativeEvent instanceof TouchEvent) {
-        const touches = nativeEvent.touches
-
-        if (Boolean(touches) && touches.length === 1) {
-          newState.startX = touches[0].clientX
-          newState.startY = touches[0].clientY
-        }
-      } else if (nativeEvent instanceof MouseEvent) {
-        newState.startX = nativeEvent.clientX
-        newState.startY = nativeEvent.clientY
+      if (Boolean(touches) && touches.length === 1) {
+        stateRef.current.startX = touches[0].clientX
+        stateRef.current.startY = touches[0].clientY
       }
-      newState.isRealEvent = true
-      onStartTouch()
-
-      return newState
-    })
+    } else if (nativeEvent instanceof MouseEvent) {
+      stateRef.current.startX = nativeEvent.clientX
+      stateRef.current.startY = nativeEvent.clientY
+    }
+    stateRef.current.isRealEvent = true
+    onStartTouch()
   }
 
   function onTouchMove (nativeEvent: TouchEvent | MouseEvent): void {
-    setState((state: TouchManagerState) => {
-      let progressX = 0
-      let progressY = 0
+    let progressX = 0
+    let progressY = 0
 
-      if (state.isRealEvent) {
-        if (nativeEvent instanceof TouchEvent) {
-          progressX = nativeEvent.touches[0].clientX
-          progressY = nativeEvent.touches[0].clientY
-        } else if (nativeEvent instanceof MouseEvent) {
-          progressX = nativeEvent.clientX
-          progressY = nativeEvent.clientY
-        }
-
-        progressX = Math.round(progressX) - state.startX
-        progressY = Math.round(progressY) - state.startY
-
-        if (Math.abs(progressX) > Math.abs(progressY)) {
-          onMoveTouch(progressX)
-        }
+    if (stateRef.current.isRealEvent) {
+      if (nativeEvent instanceof TouchEvent) {
+        progressX = nativeEvent.touches[0].clientX
+        progressY = nativeEvent.touches[0].clientY
+      } else if (nativeEvent instanceof MouseEvent) {
+        progressX = nativeEvent.clientX
+        progressY = nativeEvent.clientY
       }
 
-      return state
-    })
+      progressX = Math.round(progressX) - stateRef.current.startX
+      progressY = Math.round(progressY) - stateRef.current.startY
+
+      if (Math.abs(progressX) > Math.abs(progressY)) {
+        onMoveTouch(progressX)
+      }
+    }
   }
 
   function onTouchEnd (nativeEvent: TouchEvent | MouseEvent): void {
-    setState((state: TouchManagerState) => {
-      const newState = { ...state }
-      let finalProgressX = 0
-      let finalProgressY = 0
+    let finalProgressX = 0
+    let finalProgressY = 0
 
-      if (nativeEvent instanceof TouchEvent) {
-        finalProgressX = nativeEvent.changedTouches[0].clientX
-        finalProgressY = nativeEvent.changedTouches[0].clientY
-      } else if (nativeEvent instanceof MouseEvent) {
-        finalProgressX = nativeEvent.clientX
-        finalProgressY = nativeEvent.clientY
-      }
+    if (nativeEvent instanceof TouchEvent) {
+      finalProgressX = nativeEvent.changedTouches[0].clientX
+      finalProgressY = nativeEvent.changedTouches[0].clientY
+    } else if (nativeEvent instanceof MouseEvent) {
+      finalProgressX = nativeEvent.clientX
+      finalProgressY = nativeEvent.clientY
+    }
 
-      finalProgressX = (finalProgressX) - newState.startX
-      finalProgressY = (finalProgressY) - newState.startY
-      newState.isRealEvent = false
+    finalProgressX = (finalProgressX) - stateRef.current.startX
+    finalProgressY = (finalProgressY) - stateRef.current.startY
+    stateRef.current.isRealEvent = false
 
-      if (Math.abs(finalProgressX) > MIN_SENSI && Math.abs(finalProgressX) > Math.abs(finalProgressY)) {
-        onPageChange(finalProgressX)
-      } else if (finalProgressX !== 0) {
-        onResetColumns()
-      }
-
-      return newState
-    })
+    if (Math.abs(finalProgressX) > MIN_SENSI && Math.abs(finalProgressX) > Math.abs(finalProgressY)) {
+      onPageChange(finalProgressX)
+    } else if (finalProgressX !== 0) {
+      onResetColumns()
+    }
   }
 
   return children
