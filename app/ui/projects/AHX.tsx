6b8bcfecb9114df8fd9192adb24d5a1e028b489a
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2024-05-25 02:51:49 +0200

    Corrected AHX tags margin

--- app/ui/projects/AHX.tsx
+++ app/ui/projects/AHX.tsx
@@ -11,8 +11,8 @@ function AHX () {
       <p className='p-5'>
         <IconLink imageClassName='float-left m-4' url="https://github.com/neuroflip/AhxMidiPlayer" iconImage="/logoGithub.png" iconAlt='github logo'></IconLink>
         AMP is a try to get some MIDI features using the AHX engine sounds at Commodore Amiga Range of computers. You can assign midi channels to ahx instruments, bang notes using NOTE ON midi messages or edit ahx instrument parameters like the ADSR curve using CC midi commands.
+        <AHXTags></AHXTags>
       </p>
-      <AHXTags></AHXTags>
     </>
   </Project>
 
