c5381b047f4a15b1e0dc20f649b637f9ebd2cc0c
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2024-05-25 10:53:21 +0200

    Corrected hidration error because bad HTML formation

--- app/ui/projects/AHX.tsx
+++ app/ui/projects/AHX.tsx
@@ -8,11 +8,11 @@ function AHX () {
     title="AHX Midi Player"
     image="/projectAHX.png">
     <>
-      <p className='p-5'>
+      <div className='p-5'>
         <IconLink imageClassName='float-left m-4' url="https://github.com/neuroflip/AhxMidiPlayer" iconImage="/logoGithub.png" iconAlt='github logo'></IconLink>
         AMP is a try to get some MIDI features using the AHX engine sounds at Commodore Amiga Range of computers. You can assign midi channels to ahx instruments, bang notes using NOTE ON midi messages or edit ahx instrument parameters like the ADSR curve using CC midi commands.
         <AHXTags></AHXTags>
-      </p>
+      </div>
     </>
   </Project>
 
