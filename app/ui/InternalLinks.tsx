da9af594432ddb7cb288bd98b165c3cfbeb60616
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2024-09-26 15:58:24 +0200

    Changed interests order in page and changed to the last cv

--- app/ui/InternalLinks.tsx
+++ app/ui/InternalLinks.tsx
@@ -3,8 +3,8 @@ import * as react from 'react'
 
 function InternalLinks() {
   return <div className="text-center text-cyan-400 text-sm mt-8 w-full mx-auto">
-    <Link href="#introduction">INTRODUCTION</Link> - <Link href="#projects">PROJECTS</Link> 
-    - <Link href="#experiments">EXPERIMENTS</Link> - <Link href="#interests">INTERESTS</Link>
+    <Link href="#introduction">INTRODUCTION</Link> - <Link href="#interests">INTERESTS</Link>
+     - <Link href="#projects">PROJECTS</Link> - <Link href="#experiments">EXPERIMENTS</Link>
   </div>
 }
 
