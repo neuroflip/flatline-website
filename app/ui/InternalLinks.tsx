43743d0c049f5bd7cdb23c76a57d530c7a6922b6
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2024-09-26 16:02:00 +0200

    use tex-lg instead

--- app/ui/InternalLinks.tsx
+++ app/ui/InternalLinks.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import * as react from 'react'
 
 function InternalLinks() {
-  return <div className="text-center text-cyan-400 text-sm mt-8 w-full mx-auto">
+  return <div className="text-center text-cyan-400 text-lg mt-8 w-full mx-auto">
     <Link href="#introduction">INTRODUCTION</Link> - <Link href="#interests">INTERESTS</Link>
      - <Link href="#projects">PROJECTS</Link> - <Link href="#experiments">EXPERIMENTS</Link>
   </div>
